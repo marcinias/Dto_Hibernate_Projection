6deb15b65da1da9f2cdf22f024ab7790ada6bff8
marcinias <marcinias@sixfaces.pl>
2020-11-20 13:22:05 +0100

    Dto Hibernate

---
+++ src/test/java/pl/sixfaces/relationship_db/RelationshipDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sixfaces.relationship_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
